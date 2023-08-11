eea95bdf818bbec5b2d0333ca219c5e81cf94db0
Rashmi Chandrashekar <rashmy@microsoft.com>
2023-08-11 16:06:30 -0700

    pass build

--- pkg/apis/monitoring/v1/register.go
+++ pkg/apis/monitoring/v1/register.go
@@ -26,7 +26,7 @@ import (
 
 // var SchemeGroupVersion = schema.GroupVersion{Group: "azmonitoring.coreos.com", Version: Version}
 
-// var SchemeGroupVersion = schema.GroupVersion{}
+var SchemeGroupVersion = schema.GroupVersion{}
 
 // Resource takes an unqualified resource and returns a Group qualified GroupResource
 func Resource(resource string) schema.GroupResource {
