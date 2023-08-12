6f27987b382a877eaf93f47eb29a47ca68c7f09d
Rashmi Chandrashekar <rashmy@microsoft.com>
2023-08-11 18:14:39 -0700

    add os import

--- pkg/apis/monitoring/v1/register.go
+++ pkg/apis/monitoring/v1/register.go
@@ -19,6 +19,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"os"
 )
 
 var PackageGroupName = func() string {
